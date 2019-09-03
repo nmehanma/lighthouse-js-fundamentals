5d0a61888c9d8246a9f835332cfe42ebd979d0d4
Najeam <nmehanma@gmal.com>
2019-09-03 03:31:57 +0000

    testing for while and backwards for loops

--- ingredients.js
+++ ingredients.js
@@ -14,4 +14,16 @@ while (i < ingredients) {
 
 // Write a for loop that prints out the contents of ingredients:
 
+console.log( ingredients);
+
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+console.log(ingredients);
+
+for (let i = ingredients.length - 1; i >= 0; i--){
+  console.log(ingredients[i]);
+}
