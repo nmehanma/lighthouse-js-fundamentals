4a6a853e551e7669c9c35ccc4d879ea04b2b2948
Najeam <nmehanma@gmal.com>
2019-09-04 07:57:34 +0000

    practice

--- concatenate-arrays.js
+++ concatenate-arrays.js
@@ -1,4 +1,12 @@
-function concat(x, y){
-  
-
+function concat(a, b) {
+  for (var i = 0; i < b.length; i++) {
+   a.push(b[i]);
+    
+  }
+  return a;
 }
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
+  
